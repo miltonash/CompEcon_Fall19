078359187ee6c76e718c4eb24d83061c1c5ebd0f
miltonash <Milton.Straw@grad.moore.sc.edu>
2019-09-12 12:38:58 -0400

    submitting problem set 2

---
+++ ProblemSets/ProblemSet2/calculator.py
@@ -0,0 +1,29 @@
+def sum_of_arguments(x, y):
+    '''
+    This is a summation of two arguments.
+
+    Args:
+        x: the first number chosen.
+        y: the second number chosen.
+
+    Returns:
+        sum: addition of the two selected arguments.
+    '''
+    sum = x + y
+    return sum
+
+def product_of_arguments(x, y):
+    '''
+    This is a product of two arguments.
+
+    Args:
+        x: the first number chosen.
+        y: the second number chosen.
+
+    Returns:
+        prod: mulitplication of the two selected arguments.
+    '''
+    prod = x * y
+    return prod
+
+from math import sqrt
